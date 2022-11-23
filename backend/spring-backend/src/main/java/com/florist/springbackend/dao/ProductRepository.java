c6a77a4512ef35c6e96771b58e521936f0f134d8
stealthoust <stealthoust@gmail.com>
2022-11-23 20:47:11 +0100

    backend products of category
    Getting products of category id

--- backend/spring-backend/src/main/java/com/florist/springbackend/dao/ProductRepository.java
+++ backend/spring-backend/src/main/java/com/florist/springbackend/dao/ProductRepository.java
@@ -1,9 +1,14 @@
 package com.florist.springbackend.dao;
 
 import com.florist.springbackend.Entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product,Integer> {
+
+    Page<Product> findByCategoryId(@Param("id") Integer id, Pageable pageable);
 }
