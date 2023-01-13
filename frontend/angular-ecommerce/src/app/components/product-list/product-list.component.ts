14a7296d42039d9452799bbbc7c316256ddb5b15
stealthoust <stealthoust@gmail.com>
2023-01-13 09:54:40 +0100

    adding product to shopping cart, update design of buttons in detail view

--- frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Product } from 'src/app/common/product/product';
-import { ProductService } from 'src/app/services/product/product.service';
+import {Component, OnInit} from '@angular/core';
+import {Product} from 'src/app/common/product/product';
+import {ProductService} from 'src/app/services/product/product.service';
 
-import { ActivatedRoute } from '@angular/router';
+import {ActivatedRoute} from '@angular/router';
+import {CartService} from "../../services/cart/cart.service";
+import {CartItem} from "../../common/cart-item/cart-item";
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -10,53 +13,63 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProductListComponent implements OnInit {
 
-  products:Product[]=[];
-  currentCategoryId:number=1;
-  searchMode:boolean=false;
+  products: Product[] = [];
+  currentCategoryId: number = 1;
+  searchMode: boolean = false;
 
-  constructor(private productService:ProductService, private route:ActivatedRoute) { }
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService,
+    private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(()=>{
+    this.route.paramMap.subscribe(() => {
       this.listProducts();
     });
- 
+
   }
+
   listProducts() {
 
-    this.searchMode=this.route.snapshot.paramMap.has('keyword');
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
 
-    if(this.searchMode){
+    if (this.searchMode) {
       this.handleSearchProducts()
+    } else {
+      this.handleListProducts();
     }
-    else{
-this.handleListProducts();
+  }
+
+
+  handleListProducts() {
+
+    const ifCategoryExsist: boolean = this.route.snapshot.paramMap.has('id');
+    if (ifCategoryExsist) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      this.currentCategoryId = 1;
     }
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
+      data => {
+        this.products = data;
+      }
+    )
   }
 
+  handleSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+  }
 
-handleListProducts(){
-
-  const ifCategoryExsist:boolean=this.route.snapshot.paramMap.has('id');
-  if(ifCategoryExsist){
-    this.currentCategoryId=+this.route.snapshot.paramMap.get('id')!;
+  addToCart(product: Product) {
+    console.log(`Adding to cart: ${product.name}`)
+    let theCartItem=new CartItem(product.id, product.name,product.imageUrl,
+      product.unitPrice)
+    this.cartService.addToCart(theCartItem);
   }
-  else{
-    this.currentCategoryId=1;
-  }
-  
-      this.productService.getProductList(this.currentCategoryId).subscribe(
-        data=>{
-          this.products=data;
-        }
-      )
-}
-handleSearchProducts() {
-const theKeyword:string=this.route.snapshot.paramMap.get('keyword')!;
-this.productService.searchProducts(theKeyword).subscribe(
-  data=>{
-    this.products=data;
-  }
-);
-}
 }
