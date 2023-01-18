b2dc95b752f268d9bd3e1f89de9fd23145c8ee4d
stealthoust <stealthoust@gmail.com>
2023-01-18 15:56:59 +0100

    review order data

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
+import {CartService} from "../../services/cart/cart.service";
 
 @Component({
   selector: 'app-checkout',
@@ -9,8 +10,11 @@ import {FormBuilder, FormGroup} from "@angular/forms";
 export class CheckoutComponent implements OnInit {
 
   checkoutFormGroup!:FormGroup;
+  totalQuantity:number=0;
+  totalPrice:number=0;
 
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(private formBuilder:FormBuilder,
+              private cartService:CartService) { }
 
   ngOnInit(): void {
     this.checkoutFormGroup=this.formBuilder.group({
@@ -18,7 +22,41 @@ export class CheckoutComponent implements OnInit {
         firstName:[''],
         lastName:[''],
         email:['']
+      }),
+      shippingAddress:this.formBuilder.group({
+        country:[''],
+        state:[''],
+        city:[''],
+        zipCode:[''],
+        street:['']
+      }),
+      creditCard:this.formBuilder.group({
+        cardType:[''],
+        nameOnCard:[''],
+        cardNumber:[''],
+        securityCode:[''],
+        expMonth:[''],
+        expYear:['']
       })
-    });
+
+      });
+
+    this.getCartDetails();
+    }
+
+  getCartDetails(){
+    this.cartService.totalQuantity.subscribe(data=>{
+      this.totalQuantity=data;
+    })
+    this.cartService.totalPrice.subscribe(data=>{
+      this.totalPrice=data;
+
+    })
+  }
+
+  onSubmit(){
+    console.log("Handling the submit button");
+    console.log(this.checkoutFormGroup.get('customer')?.value);
+    console.log(this.checkoutFormGroup.get('creditCard')?.get('cardType')?.value);
   }
 }
